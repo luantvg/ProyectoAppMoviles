86edddbc268fc4bd29e4b5679e3b1c34095f83b8
LV <LuisVilla@macbook-pro-de-laura.local>
2018-10-29 11:23:51 -0600

    dos dos

--- Tablas201711/PanoramicViewController.swift
+++ Tablas201711/PanoramicViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class PanoramicViewController: UIViewController {
+    
+    let panoramaView = CTPanoramaView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let url = URL(string: "http://www.panotools.org/dersch/StBp.JPG")
+        let data = try? Data(contentsOf: url!)
+        let image = UIImage(data: data!)
+        panoramaView.image = image
+       
         // Do any additional setup after loading the view.
     }
     
