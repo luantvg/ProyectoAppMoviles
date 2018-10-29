bfeea0460bc047daa1a1cb25dda990aefce1a839
LV <LuisVilla@macbook-pro-de-laura.local>
2018-10-29 11:58:53 -0600

    PanoramicFirstApproach

--- Tablas201711/PanoramicViewController.swift
+++ Tablas201711/PanoramicViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class PanoramicViewController: UIViewController {
     
-    let panoramaView = CTPanoramaView()
+    @IBOutlet weak var panoramaView: CTPanoramaView!
+    let panoramicView = CTPanoramaView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = URL(string: "http://www.panotools.org/dersch/StBp.JPG")
+        let url = URL(string: "http://martinmolina.com.mx/201813/images/CEDETEC/101Panono%20-%20Cabina%20de%20control.jpg")
         let data = try? Data(contentsOf: url!)
         let image = UIImage(data: data!)
         panoramaView.image = image
