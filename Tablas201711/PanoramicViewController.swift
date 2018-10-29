fb5942b520e714b95d400f0768bc353536893116
LV <LuisVilla@macbook-pro-de-laura.local>
2018-10-29 10:22:10 -0600

    Holi

---
+++ Tablas201711/PanoramicViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PanoramicViewController.swift
+//  Tablas201711
+//
+//  Created by LV on 10/29/18.
+//  Copyright © 2018 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class PanoramicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
